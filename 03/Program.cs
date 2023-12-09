ad65418ef2ca9c465c0c67fe970c79abe2fe3ef7
Jasand <smileonyourface@yandex.com>
2023-12-10 01:04:24 +0500

    Task 3

---
+++ 03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+int[] array = { 20, 11, 84, 100, 1 }; // Исходный массив
+// Вывод исходного массива
+Console.Write("Правильный массив: ");
+foreach (int element in array) Console.Write(element + " ");
+// Реверсирование массива
+int a;
+for (int i = 0; i < array.Length / 2; i++)
+{
+    // Меняем местами элементы
+    a = array[i];
+    array[i] = array[array.Length - 1 - i];
+    array[array.Length - 1 - i] = a;
+}
+// Вывод измененного массива
+Console.Write("\nМассив наоборот: ");
+foreach (int el in array) Console.Write(el + " ");
